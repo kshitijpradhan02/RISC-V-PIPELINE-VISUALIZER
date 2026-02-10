73dbf4a3f3c2c40169bfb3b49fa181e013848e9b
Kshitij Pradhan <96765709+kshitijpradhan02@users.noreply.github.com>
2026-02-10 10:47:51 +0530

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,10 +1,11 @@
-#include <QApplication>
 #include "mainwindow.h"
+
+#include <QApplication>
 
 int main(int argc, char *argv[])
 {
-    QApplication app(argc, argv);
-    MainWindow window;
-    window.show();
-    return app.exec();
+    QApplication a(argc, argv);
+    MainWindow w;
+    w.show();
+    return a.exec();
 }
